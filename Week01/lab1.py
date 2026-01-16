231e5b7dbd4a10450159fdf3795e1404f5940970
kamranomars-sudo <kamran.omar@georgebrown.ca>
2026-01-16 17:34:39 -0500

    add q1,q2,q3,q4.q5

---
+++ Week01/lab1.py
@@ -0,0 +1,24 @@
+# Sample Coding Questions 01 Week 01
+# Kamran Omar
+
+# Question 02
+my_array = [1, 4, 5, 9, 3]
+
+# Question 3 : Order of Operetions
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed version of : e = a-b **c//d +a%c
+e = (a - ((b**c) // d)) + (a % c)
+
+# Question 4 : formatting
+temperature = 32.6
+
+print("The temperature today is: {:.3f} degrees Celsius.".format(temperature))
+
+# Question 5 : User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age", userAge)
